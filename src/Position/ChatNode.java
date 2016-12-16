9635c34faf615013b4cde80ae2f3991b133f531e
yyh16 <yuanyahui2009@126.com>
2016-12-16 23:58:37 +0800

    Add files via upload

---
+++ src/Position/ChatNode.java
@@ -0,0 +1,15 @@
+package Position;
+
+public class ChatNode {
+	public String id;
+	public String text;
+	
+	public ChatNode(){
+		id=null;
+		text=null;
+	}
+	public ChatNode(String id,String text){
+		this.id=id;
+		this.text=text;
+	}
+}
